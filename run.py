0bd847b1e6452ac4c184cf86f68e6461ecb7cb7b
Linushf <linus.furubom@gmail.com>
2021-12-27 22:08:48 +0000

    Check spelling and diffchecker

--- run.py
+++ run.py
@@ -7,15 +7,15 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file("creds.json")
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("love_sandwiches")
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
     """
     print("Please enter sales data from the last market.")
     print("Data should be six numbers, separated by commas.")
